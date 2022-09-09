0afba6edd81ef96666e5c946ac09cc1dacaf7f80
TiagoTNX <tiagotnx@gmail.com>
2022-09-09 01:34:33 -0300

    :recycle: Refatoração do domain com a utilização do lombok.

--- src/main/java/br/com/adopet/adocao/domain/Animal.java
+++ src/main/java/br/com/adopet/adocao/domain/Animal.java
@@ -1,11 +1,18 @@
 package br.com.adopet.adocao.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,47 +22,4 @@ public class Animal {
     private Long idade;
     private String cidade;
     private String descricao;
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public Long getIdade() {
-        return idade;
-    }
-    public void setIdade(Long idade) {
-        this.idade = idade;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 }
--- src/main/java/br/com/adopet/adocao/domain/Usuario.java
+++ src/main/java/br/com/adopet/adocao/domain/Usuario.java
@@ -1,11 +1,18 @@
 package br.com.adopet.adocao.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,48 +22,4 @@ public class Usuario {
     private String cidade;
     private String descricao;
     private String email;
-
-
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Long getTelefone() {
-        return telefone;
-    }
-    public void setTelefone(Long telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
