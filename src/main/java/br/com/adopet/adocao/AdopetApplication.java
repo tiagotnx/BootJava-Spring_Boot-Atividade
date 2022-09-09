4c42664b375f6369a83223e0766a391c2b078436
TiagoTNX <tiagotnx@gmail.com>
2022-09-08 23:32:07 -0300

    :tada: Initial commit.

---
+++ src/main/java/br/com/adopet/adocao/AdopetApplication.java
@@ -0,0 +1,13 @@
+package br.com.adopet.adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdopetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdopetApplication.class, args);
+	}
+
+}
